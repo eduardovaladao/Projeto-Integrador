38e45a770f6ab55d10e4424ab9fffbccfa339b0e
Your Name <you@example.com>
2023-09-20 09:30:15 -0300

    a

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
 import domain.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Date data1 = new SimpleDateFormat("dd/MM/yyyy").parse("12/03/1995");
+
         Recepcao recepcao = new Recepcao(1, "+55 34 3222-6724", null, null);
 
-        Paciente paciente = new Paciente(1, "Lucas Matheus", "123.456.789-10", 'M', "RJ-70.102.031", null, "+55 34 96543-8987", "lusca@gmail.com", null);
+        Paciente paciente = new Paciente(1, "Lucas Matheus", "123.456.789-10", 'M', "RJ-70.102.031", data1, "+55 34 96543-8987", "lusca@gmail.com", null);
 
         Medico medico = new Medico(2, "Samuel Prado", "111.213.141-51", 'M', "SP-92.164.369-", null, "74.457/MG", "Cardiologista", null);
         
